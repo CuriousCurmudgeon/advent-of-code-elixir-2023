ea2c3121a051f10446416a44ba72b8696199dc4c
Brian Meeker <brian@brianmeeker.me>
2023-12-02 17:46:21 -0500

    Day 2: Parse the game into a data structure
    This outputs a list of tuples of the form
    
    {1,
      [
        %{"blue" => 3, "red" => 4},
        %{"blue" => 6, "green" => 2, "red" => 1},
        %{"green" => 2}
      ]}
    
    I'm keeping the string keys for now. I could convert them to atoms at
    some point for efficiency. This is accepting external input, but I know
    the structure of that input, so atom exhaustion isn't an issue.

--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(&parse_game/1)
+    |> Enum.filter(fn x -> x end)
+    |> dbg()
   end
 
   def part2(_args) do
+  end
+
+  defp parse_game(""), do: nil
+
+  defp parse_game(input) do
+    [game_string | handful_strings] = String.split(input, [":", ";"])
+    game_id = game_string |> String.split() |> List.last() |> Integer.parse() |> elem(0)
+
+    handfuls =
+      handful_strings
+      |> Enum.map(fn h -> Regex.scan(~r/\d\s[a-z]+/, h) |> List.flatten() end)
+      |> Enum.map(&parse_colors/1)
+
+    {game_id, handfuls}
+  end
+
+  defp parse_colors(color_string_list) do
+    color_string_list
+    |> Enum.reduce(%{}, fn x, map ->
+      {color, count} = parse_color(x)
+      Map.put(map, color, count)
+    end)
+  end
+
+  defp parse_color(color_string) do
+    [count_string, color] = String.split(color_string)
+    {color, Integer.parse(count_string) |> elem(0)}
   end
 end
