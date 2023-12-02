9d43310834c3d355f28467d560fc5ad12340ffe3
Brian Meeker <brian@brianmeeker.me>
2023-12-02 13:59:53 -0500

    Split out a separate function to parse each line
    Also converted to a clean call to Enum.reduce

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,11 +1,16 @@
 defmodule AdventOfCode.Day01 do
   def part1(input) do
     String.split(input)
-    |> Enum.map(fn x -> String.replace(x, ~r/[^0-9]/, "") end)
-    |> Enum.map(fn x -> (String.first(x) <> String.last(x)) |> Integer.parse() |> elem(0) end)
-    |> Enum.sum()
+    |> Enum.reduce(0, &(calibrate(&1) + &2))
   end
 
   def part2(_args) do
+  end
+
+  defp calibrate(input) do
+    input
+    |> String.replace(~r/[^0-9]/, "")
+    |> then(fn x -> (String.first(x) <> String.last(x)) |> Integer.parse() end)
+    |> elem(0)
   end
 end
