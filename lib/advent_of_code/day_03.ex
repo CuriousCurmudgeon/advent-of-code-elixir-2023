236cdf93439fdb093fbdab91390edcd720e72e32
Brian Meeker <brian@brianmeeker.me>
2023-12-03 15:19:06 -0500

    Day 3: Making a big mess for part 2
    I'm parsing the data in a way that makes sense. I have a list with the
    position of each star and a map with tuple keys for each coordinate
    containing a number. Looks like
    
    %{
      {0, 0} => 467,
      {0, 1} => 467,
      {0, 2} => 467,
      {0, 5} => 114,
      {0, 6} => 114,
      {0, 7} => 114,
      {2, 2} => 35,
      {2, 3} => 35,
      {2, 6} => 633,
      {2, 7} => 633,
      {2, 8} => 633,
      {4, 0} => 617,
      {4, 1} => 617,
      {4, 2} => 617,
      {5, 7} => 58,
      {5, 8} => 58,
      {6, 2} => 592,
      {6, 3} => 592,
      {6, 4} => 592,
      {7, 6} => 755,
      {7, 7} => 755,
      {7, 8} => 755,
      {9, 1} => 664,
      {9, 2} => 664,
      {9, 3} => 664,
      {9, 5} => 598,
      {9, 6} => 598,
      {9, 7} => 598
    }

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,11 +1,12 @@
 defmodule AdventOfCode.Day03 do
+  ##########
+  # Part 1 #
+  ##########
   def part1(input) do
     {digits, symbols} =
       input
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.with_index()
-      |> Enum.map(&parse_line/1)
+      |> prep_input()
+      |> Enum.map(&parse_part1_line/1)
       |> Enum.reduce({[], []}, fn {digits, symbols}, {digits_acc, symbols_acc} ->
         {digits ++ digits_acc, symbols ++ symbols_acc}
       end)
@@ -18,38 +19,85 @@ defmodule AdventOfCode.Day03 do
     |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp parse_part1_line({line, index}) do
+    {parse_digits(line, index), parse_symbols(line, index)}
   end
 
-  defp parse_line({line, index}) do
-    digits =
-      Regex.scan(~r/\d+/, line, return: :index)
-      |> List.flatten()
-      |> Enum.map(fn {start, length} ->
-        {get_number_from_line(line, start, length), {index, start}}
+  defp parse_symbols(line, index) do
+    Regex.scan(~r/[^\d.]/, line, return: :index)
+    |> List.flatten()
+    |> Enum.map(fn {start, _} -> {index, start} end)
+  end
+
+  defp is_part_number({num, {line, index}}, symbols_mapset) do
+    num_length = num_length(num)
+
+    (line - 1)..(line + 1)
+    |> Enum.any?(&line_matches(&1, index, num_length, symbols_mapset))
+  end
+
+  ##########
+  # Part 2 #
+  ##########
+  def part2(input) do
+    {digits, stars} =
+      input
+      |> prep_input()
+      |> Enum.map(&parse_part2_line/1)
+      |> Enum.reduce({[], []}, fn {digits, stars}, {digits_acc, stars_acc} ->
+        {digits ++ digits_acc, stars ++ stars_acc}
       end)
 
-    symbols =
-      Regex.scan(~r/[^\d.]/, line, return: :index)
-      |> List.flatten()
-      |> Enum.map(fn {start, _} -> {index, start} end)
+    digits_map =
+      digits
+      |> Enum.reduce(%{}, fn {digit, {line, index}}, acc ->
+        index..(index + num_length(digit) - 1)
+        |> Enum.reduce(%{}, fn i, a -> Map.put(a, {line, i}, digit) end)
+        |> Map.merge(acc)
+      end)
+  end
 
-    {digits, symbols}
+  defp parse_part2_line({line, index}) do
+    {parse_digits(line, index), parse_stars(line, index)}
+  end
+
+  defp parse_stars(line, index) do
+    Regex.scan(~r/\*/, line, return: :index)
+    |> List.flatten()
+    |> Enum.map(fn {start, _} -> {index, start} end)
+  end
+
+  defp is_gear({num, {line, index}}, gears_mapset) do
+  end
+
+  ##########
+  # Shared #
+  ##########
+  defp prep_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.with_index()
+  end
+
+  defp parse_digits(line, index) do
+    Regex.scan(~r/\d+/, line, return: :index)
+    |> List.flatten()
+    |> Enum.map(fn {start, length} ->
+      {get_number_from_line(line, start, length), {index, start}}
+    end)
   end
 
   defp get_number_from_line(line, start, length) do
     String.slice(line, start, length) |> String.to_integer()
   end
 
-  defp is_part_number({num, {line, index}}, symbols_mapset) do
-    num_length = num |> Integer.digits() |> length()
-
-    (line - 1)..(line + 1)
-    |> Enum.any?(&line_matches(&1, index, num_length, symbols_mapset))
-  end
-
   defp line_matches(line, index, num_length, symbols_mapset) do
     (index - 1)..(index + num_length)
     |> Enum.any?(fn i -> MapSet.member?(symbols_mapset, {line, i}) end)
+  end
+
+  defp num_length(num) do
+    num |> Integer.digits() |> length()
   end
 end
