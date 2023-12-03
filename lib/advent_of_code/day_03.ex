61be5479628422b1d6c6c67d7f536c627e65e8f2
Brian Meeker <brian@brianmeeker.me>
2023-12-03 14:38:21 -0500

    Day 3: Solution to part 1 is done
    Needs some refactoring, but it got the correct solution. Not quite sure
    yet how well this set me up for part 2.

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,19 +1,26 @@
 defmodule AdventOfCode.Day03 do
   def part1(input) do
-    input
-    |> String.trim()
-    |> String.split("\n")
-    |> Enum.with_index()
-    |> Enum.map(&parse_line/1)
-    |> dbg()
+    {digits, symbols} =
+      input
+      |> String.trim()
+      |> String.split("\n")
+      |> Enum.with_index()
+      |> Enum.map(&parse_line/1)
+      |> Enum.reduce({[], []}, fn {digits, symbols}, {digits_acc, symbols_acc} ->
+        {digits ++ digits_acc, symbols ++ symbols_acc}
+      end)
+
+    symbols_mapset = MapSet.new(symbols)
+
+    digits
+    |> Enum.filter(&is_part_number(&1, symbols_mapset))
+    |> Enum.map(fn {num, _} -> num end)
+    |> Enum.sum()
   end
 
   def part2(_args) do
   end
 
-  # Returns a list of lists of the form
-  # [[{-1, 0}, {5, 1}], [{7, 2}, {-1, 0}]]
-  # See docs for Regex.scan/3 for details
   defp parse_line({line, index}) do
     digits =
       Regex.scan(~r/\d+/, line, return: :index)
@@ -32,5 +39,25 @@ defmodule AdventOfCode.Day03 do
 
   defp get_number_from_line(line, start, length) do
     String.slice(line, start, length) |> String.to_integer()
+  end
+
+  defp is_part_number({num, {line, index}}, symbols_mapset) do
+    num_length = num |> Integer.digits() |> length()
+    # Check on line above at all character positions from index - 1 to index + 1
+    symbol_above =
+      (index - 1)..(index + num_length)
+      |> Enum.any?(fn i -> MapSet.member?(symbols_mapset, {line - 1, i}) end)
+
+    # Check index - 1 and index + 1 on current line
+    symbol_next_to =
+      MapSet.member?(symbols_mapset, {line, index - 1}) ||
+        MapSet.member?(symbols_mapset, {line, index + num_length})
+
+    # Check on line above at all character positions from index - 1 to index + 1
+    symbol_below =
+      (index - 1)..(index + num_length)
+      |> Enum.any?(fn i -> MapSet.member?(symbols_mapset, {line + 1, i}) end)
+
+    symbol_above or symbol_next_to or symbol_below
   end
 end
