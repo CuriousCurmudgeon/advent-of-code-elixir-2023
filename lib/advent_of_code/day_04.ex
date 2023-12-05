3ce41fb55dee79e2422bddf027716bc351f66c0b
Brian Meeker <brian@brianmeeker.me>
2023-12-04 19:58:46 -0500

    Day 4: Implemented part 1 of the solution
    I'm really happy with how simple this is.

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,23 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&find_winners/1)
+    |> Enum.map(&MapSet.size/1)
+    |> Enum.filter(fn x -> x > 0 end)
+    |> Enum.reduce(0, fn x, acc -> :math.pow(2, x - 1) + acc end)
+  end
+
+  defp find_winners(line) do
+    [winners, mine] =
+      line
+      |> String.split([":", "|"])
+      |> tl()
+      |> Enum.map(&String.split/1)
+      |> Enum.map(&MapSet.new/1)
+
+    MapSet.intersection(winners, mine)
   end
 
   def part2(_args) do
