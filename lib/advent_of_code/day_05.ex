780d0c10892ca6a5b7f68b0692b5f60a3b799392
Brian Meeker <brian@brianmeeker.me>
2023-12-08 16:57:08 -0500

    Day 5: Refactored to share logic between parts
    Only the seed parsing needs to change

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,37 +1,14 @@
 defmodule AdventOfCode.Day05 do
   def part1(input) do
-    [seeds | map_lines] =
-      input
-      |> String.trim()
-      |> String.split("\n")
-      |> Enum.map(&parse_line/1)
-      |> Enum.filter(& &1)
+    [seed_line | rest] = prep_input(input)
 
-    mappings =
-      map_lines
-      |> Enum.chunk_by(fn x -> x == :map end)
-      |> Enum.drop_every(2)
-
-    seeds
-    |> Enum.map(&find_location(&1, mappings))
-    |> Enum.min()
+    parse_part1_seed_line(seed_line)
+    |> solve(rest)
   end
 
-  def parse_line(line) do
-    case String.split(line, ~r/[: ]/) do
-      ["seeds", "" | numbers] ->
-        Enum.map(numbers, &String.to_integer/1)
-
-      [""] ->
-        nil
-
-      [_, "map" | _] ->
-        :map
-
-      [_, _, _] = mapping ->
-        [dest, source, range] = Enum.map(mapping, &String.to_integer/1)
-        {dest, source, range}
-    end
+  defp parse_part1_seed_line(line) do
+    ["seeds", "" | numbers] = String.split(line, ~r/[: ]/)
+    Enum.map(numbers, &String.to_integer/1)
   end
 
   def find_location(seed, mappings) do
@@ -51,5 +28,44 @@ defmodule AdventOfCode.Day05 do
   end
 
   def part2(_args) do
+  end
+
+  ##########
+  # Shared #
+  ##########
+  defp prep_input(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+  end
+
+  defp parse_line(line) do
+    case String.split(line, ~r/[: ]/) do
+      [""] ->
+        nil
+
+      [_, "map" | _] ->
+        :map
+
+      [_, _, _] = mapping ->
+        [dest, source, range] = Enum.map(mapping, &String.to_integer/1)
+        {dest, source, range}
+    end
+  end
+
+  defp solve(seeds, lines) do
+    map_lines =
+      lines
+      |> Enum.map(&parse_line/1)
+      |> Enum.filter(& &1)
+
+    mappings =
+      map_lines
+      |> Enum.chunk_by(fn x -> x == :map end)
+      |> Enum.drop_every(2)
+
+    seeds
+    |> Enum.map(&find_location(&1, mappings))
+    |> Enum.min()
   end
 end
