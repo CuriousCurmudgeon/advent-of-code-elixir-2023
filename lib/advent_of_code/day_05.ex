f55d7d5b0d89da16f9948fb0dea1e19f749203a3
Brian Meeker <brian.meeker@getthru.io>
2023-12-08 09:15:30 -0500

    Day 5: Parse the input for part 1 into parts
    Doesn't do anything with them yet. Need to chunk them by map sections next

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,5 +1,29 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.trim()
+    |> String.split("\n")
+    |> Enum.map(&parse_line/1)
+
+    # Parse the seeds line
+    # Parse each section. Delimiter is an empty line. Name of the new section is irrelevant
+  end
+
+  def parse_line(line) do
+    case String.split(line, ~r/[: ]/) do
+      ["seeds", "" | numbers] ->
+        {:seeds, Enum.map(numbers, &String.to_integer/1)}
+
+      [""] ->
+        nil
+
+      [_, "map" | _] ->
+        :map
+
+      [_, _, _] = mapping ->
+        [dest, source, range] = Enum.map(mapping, &String.to_integer/1)
+        {dest, source, range}
+    end
   end
 
   def part2(_args) do
